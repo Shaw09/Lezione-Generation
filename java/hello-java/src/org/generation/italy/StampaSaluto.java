42c41782d960b844ebc9c386cd83a91908b70dbd
Shaw <michele_aliffi@yahoo.it>
2021-11-12 12:05:44 +0100

    First commit

---
+++ java/hello-java/src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,12 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+	
+	// questo è il metodo che esegue la classe
+	public static void main(String[] args) {
+		// questa espressione stampa in console
+		System.out.println("Ciao Java mi piace molto");
+
+	}
+
+}
